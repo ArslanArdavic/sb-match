0fbf7cfe671cf700f918a8c627dd2a794e0210f7
Arda Arslan <arda.arslan@std.bogazici.edu.tr>
2026-06-26 23:28:21 +0300

    feat: simulate endpoint conditioned brownian bridge on tests.reference

--- diffusion/reference.py
+++ diffusion/reference.py
@@ -3,17 +3,22 @@ import torch
 def sample_brownian_state(x0, sigma, t):
     # x0 : [N,3,H,W]
     # t  : [N,]
+    # xt = x0 + sigma * sqrt_t_broadcast * N( 0 , 1) 
     sqrt_t = torch.sqrt(t)                                      # [N]
     sqrt_t_broadcast = sqrt_t.view(-1, *(1,) * (x0.dim() - 1))  # [N,1,1,1]
     eps = torch.randn_like(x0)                                  # [N,3,H,W]
-    # xt = x0 + sigma * sqrt_t_broadcast * eps : [N,3,H,W]
     return x0 + sigma * sqrt_t_broadcast * eps
 
-def simulate_diffusion(x0,t):
-    return path
+def sample_conditioned_brownian_bridge(x0, xT, sigma, t):    
+    # xt = x0 * (1-t) + xT * t + sigma * N( 0 , t(1-t) )
+    # xt = x0 * (1-t) + xT * t + sigma * N( 0 , 1) * sqrt( t(1-t) )
 
-def sample_intermediate_brownian_bridge(x0,xT,t):
-    return xt
+    sqrt_t_times_one_minus_t = torch.sqrt(t * (1-t)).view(-1, *(1,) * (x0.dim() - 1))  # [N,1,1,1]
+    
+    eps = torch.randn_like(x0) * sqrt_t_times_one_minus_t                              # [N,3,H,W]
 
-def simulate_bridge(x0,xT):
-    return path
+    t_broad = t.view(-1, *(1,) * (x0.dim() - 1))                # [N,1,1,1]
+    one_minus_t_broad = (1-t).view(-1, *(1,) * (x0.dim() - 1))  # [N,1,1,1]
+
+    return x0 * one_minus_t_broad + xT * t_broad + sigma  * eps
+
--- tests/reference.py
+++ tests/reference.py
@@ -2,7 +2,7 @@ import torch
 from torchvision.utils import save_image
 
 from utils.process_image import load_afhq_image
-from diffusion.reference import sample_brownian_state
+from diffusion.reference import sample_brownian_state, sample_conditioned_brownian_bridge
 
 
 def sample_brownian_state_test(N=5, sigma=1):
@@ -31,7 +31,42 @@ def sample_brownian_state_parallel_test(N=5, sigma=1):
     save_image(grid, f"tests/outputs/brownian_states_sigma_{sigma}_parallel.png", nrow=N + 1)
 
 
+def sample_conditioned_brownian_bridge_test(N=5, sigma=1):
+    x0 = load_afhq_image()                      # (1, 3, H, W) in [-1, 1]
+    xT = load_afhq_image(path="/home/arslan/research/literature/foundations-schrodinger-bridges-tang-2026/sb-match/data/afhq/val/wild/pixabay_wild_000840.jpg")                      
+
+    states = [x0]
+    for i in range(1, N):                       # t  in [0.2, 0.4, 0.6, 0.8] by default
+        t = torch.tensor([i / N])           
+        states.append(sample_conditioned_brownian_bridge(x0, xT, sigma, t))    
+    states.append(xT)
+
+    grid = torch.cat(states, dim=0)
+    grid = (grid + 1.0) / 2.0
+    save_image(grid, f"tests/outputs/conditioned_brownian_bridge_sigma_{sigma}.png", nrow=N + 1)
+
+def sample_conditioned_brownian_bridge_parallel_test(N=5, sigma=1):
+    x0 = load_afhq_image(downsize=512)                      # (1, 3, H, W) in [-1, 1]
+    xT = load_afhq_image(
+        path="/home/arslan/research/literature/foundations-schrodinger-bridges-tang-2026/sb-match/data/afhq/val/wild/pixabay_wild_000840.jpg",
+        downsize=512
+        )
+
+    x0_stack = x0.repeat(N-1, 1, 1, 1)                          # (N-1, 3, H, W)
+    xT_stack = xT.repeat(N-1, 1, 1, 1)                          # (N-1, 3, H, W)
+    t = torch.tensor([i / N for i in range(1, N)])              # (N-1,)
+    states = sample_conditioned_brownian_bridge(x0_stack, xT_stack, sigma, t)
+
+    grid = torch.cat([x0, states, xT], dim=0)                    # (N+1, 3, H, W)
+    grid = (grid + 1.0) / 2.0                                # [-1,1] -> [0,1]
+    save_image(grid, f"tests/outputs/conditioned_brownian_bridge_sigma_{sigma}_parallel.png", nrow=N + 1)
+
+
 if __name__ == "__main__":
+    #for sigma in [0.1, 0.5, 1, 2]:
+    #    sample_brownian_state_test(sigma=sigma)
+    #sample_brownian_state_parallel_test(sigma=0.5)
+
     for sigma in [0.1, 0.5, 1, 2]:
-        sample_brownian_state_test(sigma=sigma)
-    sample_brownian_state_parallel_test(sigma=0.5)
+        sample_conditioned_brownian_bridge_test(sigma=sigma)
+    sample_conditioned_brownian_bridge_parallel_test(sigma=1)
